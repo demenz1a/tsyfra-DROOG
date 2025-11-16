1d8db84e1c58e19765052e7594383779c948ebfd
demenz1a <demenziatwi@gmail.com>
2025-11-17 00:41:05 +0600

    Added base code
    ololoev idi k doske

---
+++ PlanetUmut.py
@@ -0,0 +1,30 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('8353976066:AAEvXMyk_Lz-pHiR15of0MANfTrnoXU-ClA')
+
+@bot.message_handler(commands=['Баштоо','start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Сурамжылоону баштоо',url='https://forms.gle/RCppbSBWsTHFFtsG7'))
+    markup.add(types.InlineKeyboardButton('Буттум',callback_data='ended'))
+    bot.reply_to(message,'Саламатсызбы! Улантуу учун баскычты басыныз',reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'ended':
+        bot.send_message(
+            callback.message.chat.id,
+            'Сурамжылоону бүтүргөн болсоңуз, заманбап тиркемелерди колдонуу боюнча жардам алуу үчүн биздин Telegram каналыбызга баш багыңыз @osh_project.'
+        )
+
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('Яндекс Такси колдонуу', url='https://t.me/c/3201978020/3'))
+        markup.add(types.InlineKeyboardButton('МБанк колдонуу', url='https://t.me/c/3201978020/4'))
+        markup.add(types.InlineKeyboardButton('Жасалма интеллект колдонуу', url='https://t.me/c/3201978020/5'))
+        markup.add(types.InlineKeyboardButton('Түндүк кирүү', url='https://t.me/c/3201978020/6'))
+        markup.add(types.InlineKeyboardButton('Түндүк колдонуу', url='https://t.me/c/3201978020/7'))
+
+        bot.send_message(callback.message.chat.id, "Төмөнкү темалар боюнча материалдар:", reply_markup=markup)
+
+bot.polling(none_stop=True)
